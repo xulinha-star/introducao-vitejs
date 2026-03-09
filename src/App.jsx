0a151ef930ec69566a68ee366db543ce44ad8a49
xulinha-star <juliacava761@gmail.com>
2026-03-09 10:55:47 -0300

    aula 0903

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   return (
-    <>
-    
-     <h1>Programação WEB III</h1>
-     <h2>Aula React com Vite JS</h2>
-     <h3>Estilização de páginas</h3>
-     <p>Usando diferentes tipos de chamada para estilização</p>
-    </>
+    <div className='container'>
+      <p>Teste</p>
+      <h1>Programação WEB III</h1>      
+      <h2>Aula React com Vite JS</h2>
+      <h3>Estilização de páginas</h3>
+      <p>Usando diferentes tipos de chamada para estilização.</p> 
+      <label>Exemplo Label</label>
+      <small>Exemplo small</small>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, repellat eum accusamus obcaecati veritatis odio possimus eveniet quos voluptatibus, similique culpa amet natus! Sit obcaecati omnis tempora in odio natus.
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis fugiat nam cupiditate aperiam mollitia repellat accusantium deserunt nesciunt eius in numquam aliquid quo voluptatibus facere molestias, repudiandae rem dicta quae?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit ipsa dignissimos excepturi, ullam, illum vitae asperiores praesentium nesciunt aspernatur laboriosam voluptatibus natus. Non, dicta exercitationem sit laudantium magnam cupiditate ipsa.
+      </p>
+    </div>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './estilizacao/reset.css'
-import './estilizacao/globals.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import App from './App.jsx';
+
+import './estilizacao/reset.css';
+import './estilizacao/globals.css';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
