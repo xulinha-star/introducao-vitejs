cf8fc019a572baa75e55abf39dcd80e9a0ae6eb4
xulinha-star <juliacava761@gmail.com>
2026-03-17 10:50:18 -0300

    Aula 17-03

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,9 +1,22 @@
-function Home(){
+import {Link} from "react-router";
+
+import styles from './index.module.css';
+
+function Home() {
     return (
-        <>
-          <h1> Aula React com VITE JS </h1>
-            
-        </>
+        <div className={styles.container}>
+            <h1>Aula React com VITE JS</h1>            
+            <div className={styles.containerItens}>
+                <h2>Exemplos</h2>
+ 
+            <Link to="/exemplo/1">Exemplo 1 - Componente básico </Link>
+            <Link to="/exemplo/2">Exemplo 2 - Uso de estilização com module </Link>
+
+            </div>
+            <div className={styles.containerItens}>
+                <h2>Atividades</h2>
+            </div>
+        </div>
     )
 }
 
